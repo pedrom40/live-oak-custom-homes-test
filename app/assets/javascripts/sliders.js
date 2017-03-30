5e2eb884d55db6788671a68fcd5f0cf082562c43
pedrom40 <pedro.morin1980@gmail.com>
2017-03-30 16:26:22 -0500

    Complete Welcome view

---
+++ app/assets/javascripts/sliders.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+  $("#mainSlider").responsiveSlides({
+    pager:false,
+    nav:true,
+    namespace:'centered-btns'
+  });
+
+  $("#welcomeSlider").responsiveSlides({
+    auto:false,
+    pager:false,
+    nav:true,
+    namespace:'centered-btns'
+  });
+
+  $("#philosophySlider").responsiveSlides({
+    auto:false,
+    pager:false,
+    nav:true,
+    namespace:'centered-btns'
+  });
+
+});

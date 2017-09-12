a445bf2f6bbf28a81a7e24de007bcea3a10570d6
pedrom40 <pedro.morin1980@gmail.com>
2017-09-12 12:23:27 -0500

    add new photos, update contact form recipient

--- app/mailers/contact_us_mailer.rb
+++ app/mailers/contact_us_mailer.rb
@@ -3,7 +3,7 @@ class ContactUsMailer < ApplicationMailer
   def contactUsSend(contact)
     @contact = contact
     @url = 'http://liveoakcustomhomes.com/contact-us'
-    mail(to: 'pedro@121texas.com', from: @contact.email, subject: 'Live Oak Custom Homes Contact Form Submission')
+    mail(to: 'casey@liveoakcustomhomes.com', from: @contact.email, subject: 'Live Oak Custom Homes Contact Form Submission')
   end
 
 end
